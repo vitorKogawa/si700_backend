6ed8c8ab4a265a1546068958a7850383941a7bb0
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-05-30 19:37:32 -0300

    removendo diretório dist

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-require("./config/env.config");
-var port = parseInt(process.env.PORT);
-app_1.app.listen(port, function () {
-    return console.log("Servidor rodando em http://localhost:" + port);
-});
-//# sourceMappingURL=server.js.map
