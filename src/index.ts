c77f7d7b03b66a2ebfba78842495406d2b07619d
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-05-30 16:15:18 -0300

    alterando o arquivo de configuração do orm

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { createConnection } from "typeorm";
 
 createConnection({
   type: "postgres",
-  url: process.env.DATABASE_URL || "postgres://raiozevmujefuh:21a94a34f033818ba5ab3636c5ee427169e2c15f7f17e4fa40076446275e9b74@ec2-34-202-54-225.compute-1.amazonaws.com:5432/de516he89l929h",
+  url: process.env.DATABASE_URL || "postgres://uwchhypexhmjsf:c82d00b314fb872bd0ad578c14d7a1759045710e3db83588b8b1ced3a73ebd2c@ec2-3-215-57-87.compute-1.amazonaws.com:5432/d6a1g732ta78ri",
   ssl: process.env.DATABASE_URL ? true : false,
   extra: {
     ssl: {
