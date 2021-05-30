eef12074eed0f8cdd31504b3f3b00551f0493652
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-05-30 19:38:41 -0300

    gerando o build do projeto

---
+++ dist/config/env.config.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var path_1 = require("path");
+var dotenv_1 = require("dotenv");
+dotenv_1.config({ path: path_1.resolve(__dirname, "..", "..", ".env") });
+//# sourceMappingURL=env.config.js.map
---
+++ dist/entity/User.js
@@ -0,0 +1,53 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var typeorm_1 = require("typeorm");
+var bcryptjs_1 = require("bcryptjs");
+var User = /** @class */ (function () {
+    function User() {
+    }
+    User.prototype.hashPassword = function () {
+        bcryptjs_1.hashSync(this.password, 8);
+    };
+    __decorate([
+        typeorm_1.PrimaryColumn(),
+        __metadata("design:type", String)
+    ], User.prototype, "id", void 0);
+    __decorate([
+        typeorm_1.Column(),
+        __metadata("design:type", String)
+    ], User.prototype, "firstName", void 0);
+    __decorate([
+        typeorm_1.Column(),
+        __metadata("design:type", String)
+    ], User.prototype, "lastName", void 0);
+    __decorate([
+        typeorm_1.Column(),
+        __metadata("design:type", String)
+    ], User.prototype, "email", void 0);
+    __decorate([
+        typeorm_1.Column(),
+        __metadata("design:type", String)
+    ], User.prototype, "password", void 0);
+    __decorate([
+        typeorm_1.BeforeInsert(),
+        typeorm_1.BeforeUpdate(),
+        __metadata("design:type", Function),
+        __metadata("design:paramtypes", []),
+        __metadata("design:returntype", void 0)
+    ], User.prototype, "hashPassword", null);
+    User = __decorate([
+        typeorm_1.Entity()
+    ], User);
+    return User;
+}());
+exports.User = User;
+//# sourceMappingURL=User.js.map
