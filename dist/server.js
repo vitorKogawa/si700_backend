4234485e04f135131f33ed9347322d5342aeacd7
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-05-31 14:22:17 -0300

    heroku

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
+var App_1 = require("./App");
 require("./config/env.config");
 var port = parseInt(process.env.PORT);
-app_1.app.listen(port, function () {
+App_1.app.listen(port, function () {
     return console.log("Servidor rodando em http://localhost:" + port);
 });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { app } from "./app";
+import { app } from "./App";
 import "./config/env.config";
 
 const port = parseInt(process.env.PORT);
