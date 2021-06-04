bb6da286b3487b90b4341c6a57d5392d92143148
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-06-04 12:00:45 -0300

    juntando app.ts com o server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,23 @@
-import { app } from "./app";
 import "./config/env.config";
+import express from "express";
+import { routes } from "./routes/routes";
+import helmet from "helmet";
+import cors from "cors";
+import "./connection";
+
+const server = express();
+
+//middlewares
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+
+//routes
+server.use(routes);
 
 const port = parseInt(process.env.PORT);
 
-app.listen(port, () =>
+server.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
 );
+
