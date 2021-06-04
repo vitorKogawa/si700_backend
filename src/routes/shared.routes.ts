025d8695b3e04f8721bedc78023a4666cd6b5efd
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-06-04 12:03:49 -0300

    ajustes nas rotas compartilhadas

--- src/routes/shared.routes.ts
+++ src/routes/shared.routes.ts
@@ -1,8 +1,10 @@
-import { Router } from 'express';
-import { userRoutes } from './shared/user.routes';
+import { Router } from "express";
+import { userRoutes } from "./shared/user.routes";
+import AuthController from './../controller/AuthController'
 
 const sharedRoutes = Router();
 
-sharedRoutes.use('/user', userRoutes);
+sharedRoutes.use("/user", userRoutes);
+sharedRoutes.post("/login", AuthController.auth);
 
-export { sharedRoutes }
+export { sharedRoutes };
