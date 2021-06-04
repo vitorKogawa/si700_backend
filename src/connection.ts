dafdeec8830eb4afe82341c0f6b7d6af40dfb1bc
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-06-04 12:00:31 -0300

    modificando classe de conexão com banco de dados

--- src/connection.ts
+++ src/connection.ts
@@ -1,21 +1,9 @@
 import "reflect-metadata";
+import "./config/database.config";
 import { createConnection } from "typeorm";
+import { dbConfig } from './config/database.config';
 
-createConnection({
-  type: "postgres",
-  url:
-    process.env.DATABASE_URL ||
-    "postgres://uwchhypexhmjsf:c82d00b314fb872bd0ad578c14d7a1759045710e3db83588b8b1ced3a73ebd2c@ec2-3-215-57-87.compute-1.amazonaws.com:5432/d6a1g732ta78ri",
-  ssl: process.env.DATABASE_URL ? true : false,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
-  synchronize: false,
-  logging: true,
-  entities: ["dist/entity/**/*.js"],
-  migrations: ["dist/migration/**/*.js"],
-})
+
+createConnection(dbConfig.development)
   .then(() => console.log("Conectado"))
   .catch((error) => console.error(error));
